d0352bdeaecf1ce7cdf04a6d1179f0248f7f040b
sofie-rk <sofierk@stud.ntnu.no>
2021-03-11 10:20:28 +0100

    breadth-first search started

---
+++ breadth_first_search/breadth_first_search.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+//------------------------------------------------------------------------------'
+
+// C++ programs start by executing the function main
+int main()
+{
+	cout << "Hello, World!\n";
+
+}
+
+//------------------------------------------------------------------------------
